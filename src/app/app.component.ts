c7401615cac57913c30b82fd8fe5a68b3e9e2f7d
Ryan Hernandez <ryanhernandez@ou.edu>
2019-11-24 10:05:45 -0600

    Created Layout
    Created basic layout of site and added some frameworks for animating the page. Condensed all code to the app.component.html page (for now) for simplicity sake.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TweenLite } from 'gsap';
+import { ScrollMagic } from 'scrollmagic';
+import "ScrollMagic/scrollmagic/minified/plugins/debug.addIndicators.min.js";
+import 'ScrollMagic/scrollmagic/minified/plugins/animation.gsap.min.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Ryan Hernandez';
+
+  ngOnInit(): void {
+    TweenLite.from('.crop', 1, { autoAlpha: 0, x: -200 });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      [],
+      {
+        useHash: true,
+        anchorScrolling: "enabled",
+        onSameUrlNavigation: "reload",
+        scrollPositionRestoration: "enabled"
+      }),
   ],
   providers: [],
   bootstrap: [AppComponent]
